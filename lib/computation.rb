6c59205b1b2736237769c4ef627e74e38571ff7f
nanda yadav <nandayadav@gmail.com>
2010-02-02 23:29:23 -0700

    more stuff

--- lib/computation.rb
+++ lib/computation.rb
@@ -1,22 +1,31 @@
 require 'vector'
 require 'data_point'
 class Computation
-  def initialize
+  attr_accessor :iso_value, :weight
+  def initialize(options = {})
+    @iso_value = options[:iso_value]
+    @x_dimension = options[:x_dimension]
+    @y_dimension = options[:y_dimension]
+    @z_dimension = options[:z_dimension]
+    @weight = options[:weight]
   end
   
+  def find_offsets(x, y, z)
+  	index = (x+ y*@x_dimension + z*@x_dimenstion*@y_dimension)
+  end
   
   #interpolate the scalar values and get the intersection point
-  def self.interpolate_scalar(vector_a, vector_b, isovalue, first, second)
+  def interpolate_scalar(vector_a, vector_b, isovalue, first, second)
   	point = {}
-  	weight = (isovalue - first)/(second - first)
+  	@weight = (isovalue - first)/(second - first)
   	vector_a.point.each{|key, val|
-  	  point[key] = val + weight*(vector_b.point[key]) - val 
+  	  point[key] = val + @weight*(vector_b.point[key]) - val 
   	}
   	Vector.new(point)
   end
   
   #interpolate the normal vectors and get the normal vector at the intersected point
-  def self.interpolate_normal(normal_a, normal_b, weight)
+  def interpolate_normal(normal_a, normal_b, weight = @weight)
   	point = {}
   	magnitude = 0.0
     normal_a.point.each{|key, val|
@@ -30,7 +39,7 @@ class Computation
   end
   
   #Given 8-bit mask, convert into a single 
-  def self.to_decimal(bit_mask)
+  def to_decimal(bit_mask)
   	value = 0.0
   	bit_mask.each_with_index{|val, index| value += val*(2**index) }
     value.to_i
