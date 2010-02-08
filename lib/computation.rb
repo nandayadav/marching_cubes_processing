79bf0c218b844bdd41e13a24183a8c5a91902f37
nanda yadav <nandayadav@gmail.com>
2010-02-07 23:26:49 -0700

    small optimizations

--- lib/computation.rb
+++ lib/computation.rb
@@ -4,24 +4,25 @@ class Computation
   attr_accessor :iso_value, :weight
   def initialize(options = {})
     @iso_value = options[:iso_value]
+    #3D data specific vars, will be filled in after reading data file
     @x_dimension = nil
     @y_dimension = nil
     @z_dimension = nil
     @weight = nil
     @size = nil
     @file = options[:file]
-    @buffer = nil
+    #Store actual 3D raw data 
+    @buffer = ''
   end
   
   #Read the binary file data into array
   def read_file
-    buffer = ""
     f = File.new(@file, "r")
-    f.each_byte{|b| buffer << b }
+    f.each_byte{|b| @buffer << b }
     f.close
     @x_dimension, @y_dimension, @z_dimension = buffer.unpack('III') #THe first 3 int values are x, y, z dimensions
     @size = @x_dimension*@y_dimension*z_dimension
-    @buffer = buffer[0..@size-1] #Assuming unsigned char is 1 byte
+    @buffer = @buffer[0..@size-1] #Assuming unsigned char is 1 byte
   end
   
   def find_offsets(x, y, z)
@@ -32,9 +33,7 @@ class Computation
   def interpolate_scalar(vector_a, vector_b, isovalue, first, second)
     point = {}
     @weight = (isovalue - first)/(second - first)
-    vector_a.point.each{|key, val|
-      point[key] = val + @weight*(vector_b.point[key]) - val 
-    }
+    vector_a.point.each{|key, val| point[key] = val + @weight*(vector_b.point[key]) - val }
     Vector.new(point)
   end
   
@@ -42,13 +41,9 @@ class Computation
   def interpolate_normal(normal_a, normal_b, weight = @weight)
     point = {}
     magnitude = 0.0
-    normal_a.point.each{|key, val|
-      point[key] = val + weight*(normal_b.point[key] - val)
-    }
+    normal_a.point.each{|key, val| point[key] = val + weight*(normal_b.point[key] - val) }
     magnitude = Math.sqrt(point.values.inject(0){|sum, val| sum + (val**2)})
-    point.each{|key, val|
-      point[key] = val/magnitude   
-    }
+    point.each{|key, val| point[key] = val/magnitude }
     Vector.new(point)
   end
   
