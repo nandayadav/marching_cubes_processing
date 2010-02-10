aca7587c00c403b21419b6a7c44ebd48015dbbcc
nanda yadav <nandayadav@gmail.com>
2010-02-09 23:13:13 -0700

    getting started with main computational loop, added more helper methods

--- lib/computation.rb
+++ lib/computation.rb
@@ -11,8 +11,29 @@ class Computation
     @weight = nil
     @size = nil
     @file = options[:file]
-    #Store actual 3D raw data 
-    @buffer = ''
+    @indices = [] #Used to store indices of 8 vertices
+    @buffer = ''  #Store actual 3D raw data 
+  end
+  
+  def main_loop
+    interpolated_vector = Vector.new
+    interpolated_normal = Vector.new
+    normal_a = Vector.new
+    normal_b = Vector.new
+    #Loop through the @buffer
+    for k in 0..@z_dimension-1
+      for j in 0..@y_dimension-1
+        for i in 0..@x_dimension-1
+          find_offsets(i, j, k)
+          mask = compute_mask(@buffer)
+          table_index = to_decimal(mask)
+          side_counter = 0
+          data_point = DataPoint.new
+          
+        end
+      end
+    end
+    
   end
   
   #Read the binary file data into array
@@ -20,13 +41,29 @@ class Computation
     f = File.new(@file, "r")
     f.each_byte{|b| @buffer << b }
     f.close
-    @x_dimension, @y_dimension, @z_dimension = buffer.unpack('III') #THe first 3 int values are x, y, z dimensions
+    @x_dimension, @y_dimension, @z_dimension = @buffer.unpack('III') #THe first 3 int values are x, y, z dimensions
     @size = @x_dimension*@y_dimension*z_dimension
     @buffer = @buffer[0..@size-1] #Assuming unsigned char is 1 byte
   end
   
   def find_offsets(x, y, z)
-    index = (x+ y*@x_dimension + z*@x_dimenstion*@y_dimension)
+    index = (x+ y*@x_dimension + z*@x_dimension*@y_dimension)
+    @indices = []
+    @indices[0] = index
+  	@indices[1] = @indices[0] + 1
+  	@indices[3] = @indices[0] + @x_dimension
+  	@indices[2] = @indices[3] + 1
+  	@indices[4] = @indices[0] + @x_dimension*@y_dimension
+  	@indices[5] = @indices[4] + 1
+  	@indices[7] = @indices[4] + @x_dimension
+  	@indices[6] = @indices[7] + 1
+  end
+  
+  #Compute mask(array of 0/1 based on iso_value and given lookup table)
+  def compute_mask(temp_buffer)
+    mask_bits = []
+    (0..7).each{|i| mask_bits[i] = temp_buffer[@indices[i]] <= @iso_value ? 1 : 0 }
+    mask_bits.reverse
   end
   
   #interpolate the scalar values and get the intersection point
